db222807f9b97d61c4fc398fe553fd51b8d9588e
Varmo <101868197+varmoh@users.noreply.github.com>
2024-05-17 09:16:18 +0300

    Update get-last-deployed-model.sql

--- Resql/training/POST/get-last-deployed-model.sql
+++ Resql/training/POST/get-last-deployed-model.sql
@@ -1,1 +1,7 @@
-SELECT * FROM llm_trainings WHERE id=(SELECT max(id) FROM llm_trainings WHERE status = 'DEPLOYED');
+SELECT id, model_type, state, trained_date, file_name, version_number, model_version, test_report, cross_validation_report, created
+FROM llm_trainings
+WHERE id = (
+  SELECT max(id)
+  FROM llm_trainings
+  WHERE status = 'DEPLOYED'
+);
