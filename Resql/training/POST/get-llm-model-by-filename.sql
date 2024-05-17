9e74530491e8e42e6d8abe744a37a92a93f4685c
Varmo <101868197+varmoh@users.noreply.github.com>
2024-05-17 09:17:25 +0300

    Update get-llm-model-by-filename.sql

--- Resql/training/POST/get-llm-model-by-filename.sql
+++ Resql/training/POST/get-llm-model-by-filename.sql
@@ -1,8 +1,8 @@
-SELECT *
+SELECT id, model_type, state, trained_date, file_name, version_number, model_version, test_report, cross_validation_report, created
 FROM llm_trainings lt
 WHERE file_name = :fileName
-  AND created = (
-    SELECT MAX(created)
-    FROM llm_trainings
-    WHERE file_name LIKE :fileName
+AND created = (
+  SELECT MAX(created)
+  FROM llm_trainings
+  WHERE file_name LIKE :fileName
 );
