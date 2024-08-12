ac8add53276b57e905f95a50e6fd464bc6fcd8af
Varmo <101868197+varmoh@users.noreply.github.com>
2024-08-12 08:17:48 +0300

    update

--- Resql/analytics/POST/feedback-avg-feedback-to-buerokratt-chats.sql
+++ Resql/analytics/POST/feedback-avg-feedback-to-buerokratt-chats.sql
@@ -5,7 +5,7 @@ WHERE EXISTS
     (SELECT 1
      FROM message
      WHERE message.chat_base_id = chat.base_id
-       AND message.author_role = 'chatbot')
+       AND message.author_role = 'buerokratt')
 AND status = 'ENDED'
 AND created::date BETWEEN :start::date AND :end::date
 GROUP BY date_time
