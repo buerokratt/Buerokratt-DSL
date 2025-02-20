f84497f2b9f1b1c9a35990d4a80afc1b03978406
KlviG <78801020+KlviG@users.noreply.github.com>
2025-02-20 14:20:50 +0200

    Update overview-total-forwarded-chats.sql

--- Resql/analytics/POST/overview-total-forwarded-chats.sql
+++ Resql/analytics/POST/overview-total-forwarded-chats.sql
@@ -1,22 +1,16 @@
-WITH botname AS (
-    SELECT "value"
-    FROM "configuration"
-    WHERE "key" = 'bot_institution_id'
-    LIMIT 1
+WITH forwarded_chats_by_csa AS (
+    SELECT base_id, customer_support_id, customer_support_display_name, created 
+    FROM chat 
+    WHERE status = 'REDIRECTED'
+    AND customer_support_id <> '' AND customer_support_id IS NOT NULL
+    GROUP BY base_id, customer_support_id, customer_support_display_name, created
 )
-SELECT COUNT(DISTINCT base_id) filter (
-        WHERE forwarded_to IS NOT NULL
-            AND received_from <> (
-                SELECT "value"
-                FROM botname
-            )
-            AND external_id IS NULL
-    ) AS metric_value
-FROM chat
-WHERE date_trunc('day', chat.created) = date_trunc('day', current_date - '1 day'::INTERVAL)
+SELECT COUNT(DISTINCT base_id) AS metric_value
+FROM forwarded_chats_by_csa
+WHERE date_trunc('day', forwarded_chats_by_csa.created) = date_trunc('day', current_date - '1 day'::INTERVAL)
 UNION ALL
 SELECT COUNT(DISTINCT base_id) filter (
-        WHERE external_id IS NOT NULL
+        WHERE external_id IS NOT NULL AND external_id <> ''
     ) AS metric_value
 FROM chat
 WHERE date_trunc('day', chat.created) = date_trunc('day', current_date - '1 day'::INTERVAL)
