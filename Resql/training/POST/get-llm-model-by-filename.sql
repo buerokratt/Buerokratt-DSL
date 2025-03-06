0661720d0f058082eef6c55af9fcae44730b690d
Varmo <101868197+varmoh@users.noreply.github.com>
2025-03-06 13:26:37 +0200

    Delete Resql directory

--- Resql/training/POST/get-llm-model-by-filename.sql
+++
@@ -1,8 +0,0 @@
-SELECT id, model_type, state, trained_date, file_name, version_number, model_version, test_report, cross_validation_report, created, training_data_checksum
-FROM llm_trainings lt
-WHERE file_name = :fileName
-AND created = (
-  SELECT MAX(created)
-  FROM llm_trainings
-  WHERE file_name LIKE :fileName
-);
--- Resql/training/POST/get-service-intent-connections.sql
+++
@@ -1,6 +0,0 @@
-SELECT intent,
-      service,
-      MAX(service_name) AS service_name
-FROM service_trigger
-GROUP BY intent, service
-HAVING MAX(status) = 'approved';
