cf903e19f872db850185afc1029199be52230613
Varmo <101868197+varmoh@users.noreply.github.com>
2025-03-06 09:13:36 +0200

    Update chat-cif-all-csas-away.sql

--- Resql/analytics/POST/chat-cif-all-csas-away.sql
+++ Resql/analytics/POST/chat-cif-all-csas-away.sql
@@ -1,27 +1,40 @@
 WITH offlineCSAs AS (
-  SELECT COUNT(csa.id_code) AS csaCount
-  FROM customer_support_agent_activity csa
-  WHERE csa.id = (
-    SELECT MAX(id)
-    FROM customer_support_agent_activity
-    WHERE id_code = csa.id_code
-  )
-  AND csa.status = 'offline'
-  AND csa.created::date BETWEEN :start::date AND :end::date
-),
-AllCSAs AS (
-  SELECT COUNT(*) AS userCount FROM public.user pu WHERE pu.created::date NOT BETWEEN :start::date AND :end::date
-)
+    SELECT COUNT(csa.id_code) AS csaCount
+    FROM customer_support_agent_activity csa
+    WHERE csa.id = (
+        SELECT MAX(id)
+        FROM customer_support_agent_activity
+        WHERE id_code = csa.id_code
+    )
+      AND csa.status = 'offline'
+      AND csa.created::date BETWEEN :start::date AND :end::date
+    ),
+    AllCSAs AS (
+SELECT COUNT(*) AS userCount
+FROM public.user pu
+WHERE pu.created::date NOT BETWEEN :start::date AND :end::date
+    )
 SELECT
-  DATE_TRUNC(:period, c.created) AS time,
+    DATE_TRUNC(:period, c.created) AS time,
   COUNT(DISTINCT c.base_id) AS chat_count
 FROM chat c
-JOIN message m ON c.base_id = m.chat_base_id 
+    JOIN message m ON c.base_id = m.chat_base_id
 WHERE c.created::date BETWEEN :start::date AND :end::date
 AND (
-    m.event = 'unavailable-contact-information-fulfilled' AND
-    (c.end_user_email IS NOT NULL AND c.end_user_email <> '') OR
-    (c.end_user_phone IS NOT NULL AND c.end_user_phone <> '')
+    m.event = 'unavailable-contact-information-fulfilled'
+    AND (
+      (c.end_user_email IS NOT NULL AND c.end_user_email <> '')
+      OR
+      (c.end_user_phone IS NOT NULL AND c.end_user_phone <> '')
+    )
+)
+AND (
+  c.base_id IN (
+    SELECT DISTINCT m.chat_base_id
+    FROM message m
+    WHERE m.event = 'unavailable_csas_ask_contacts'
+    AND m.author_role = 'buerokratt'
   )
+)
 GROUP BY time
 ORDER BY time;
