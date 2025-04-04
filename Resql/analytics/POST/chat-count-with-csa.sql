612b4c6f2841ea751cc10c5ce97b593ce43b87db
Varmo <101868197+varmoh@users.noreply.github.com>
2025-04-04 08:27:45 +0300

    Update chat-count-with-csa.sql

--- Resql/analytics/POST/chat-count-with-csa.sql
+++ Resql/analytics/POST/chat-count-with-csa.sql
@@ -1,5 +1,4 @@
-SELECT
-    DATE_TRUNC(:period, ended) AS time,
+SELECT DATE_TRUNC(:period, ended) AS time,
     COUNT(DISTINCT base_id) AS count
 FROM chat
 WHERE ended::date BETWEEN :start::date AND :end::date AND status = 'ENDED'
@@ -8,6 +7,12 @@ AND EXISTS (
     FROM message
     WHERE message.chat_base_id = chat.base_id
     AND message.author_role = 'backoffice-user'
+)
+AND EXISTS (
+    SELECT 1
+    FROM message
+    WHERE message.chat_base_id = chat.base_id
+    and message.event = 'taken-over'
 )
 GROUP BY time
 ORDER BY time
