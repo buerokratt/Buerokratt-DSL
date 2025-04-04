05659b658a9ec06b6b3c34d2039364cfb9b00a7c
Varmo <101868197+varmoh@users.noreply.github.com>
2025-04-04 08:43:54 +0300

    Update status-all-chats.sql

--- Resql/analytics/POST/status-all-chats.sql
+++ Resql/analytics/POST/status-all-chats.sql
@@ -1,8 +1,8 @@
-SELECT date_trunc(:metric, chat.created) AS date_time, message.event AS event,
+SELECT date_trunc(:metric, chat.ended) AS date_time, message.event AS event,
        COUNT(DISTINCT chat.base_id)
 FROM chat JOIN message ON chat.base_id = message.chat_base_id
 AND status = 'ENDED'
 AND message.event IN (:events)
-AND chat.created::date BETWEEN :start::date AND :end::date
+AND chat.ended::date BETWEEN :start::date AND :end::date
 GROUP BY date_time, event
 ORDER BY event
