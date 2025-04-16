f6ad48d9c564be60c0ac06255e807df5faffab06
Varmo <101868197+varmoh@users.noreply.github.com>
2025-04-16 12:20:44 +0300

    Update feedback-avg-feedback-to-buerokratt-chats.sql

--- Resql/analytics/POST/feedback-avg-feedback-to-buerokratt-chats.sql
+++ Resql/analytics/POST/feedback-avg-feedback-to-buerokratt-chats.sql
@@ -1,11 +1,11 @@
-SELECT date_trunc(:metric, created) AS date_time,
-       ROUND(1.0 * SUM(CASE WHEN feedback_rating IS NOT NULL THEN 1 ELSE 0 END) / COUNT(DISTINCT base_id), 1) AS avg
+SELECT date_trunc(:metric, ended) AS date_time,
+       ROUND(1.0 * SUM(CASE WHEN feedback_rating IS NOT NULL THEN feedback_rating ELSE 0 END) / NULLIF(COUNT(DISTINCT base_id), 0), 1) AS avg
 FROM chat
 WHERE EXISTS
     (SELECT 1
      FROM message
      WHERE message.chat_base_id = chat.base_id
-       AND message.author_role = 'buerokratt')
+       AND (message.author_role = 'buerokratt' OR message.author_role = 'Bürokratt'))
 AND status = 'ENDED'
-AND created::date BETWEEN :start::date AND :end::date
+AND ended::date BETWEEN :start::date AND :end::date
 GROUP BY date_time
