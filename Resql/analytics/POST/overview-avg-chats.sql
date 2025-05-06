7d049256277ce10d413ac8e41b2551198a77e01c
Varmo <101868197+varmoh@users.noreply.github.com>
2025-05-06 08:29:26 +0300

    Update overview-avg-chats.sql

--- Resql/analytics/POST/overview-avg-chats.sql
+++ Resql/analytics/POST/overview-avg-chats.sql
@@ -1,6 +1,6 @@
 WITH chats AS (
     SELECT COUNT(DISTINCT base_id) AS num_chats,
-        date_trunc('day', created) AS created
+        date_trunc('day', ended) AS ended
     FROM chat
     WHERE EXISTS (
             SELECT 1
@@ -8,13 +8,13 @@ WITH chats AS (
             WHERE message.chat_base_id = chat.base_id
                 AND message.author_role = 'end-user'
         )
-        AND chat.created >= date_trunc(
+        AND chat.ended >= date_trunc(
             :group_period,
             current_date - concat('1 ', :group_period)::INTERVAL
         )
     GROUP BY 2
 )
-SELECT date_trunc(:group_period, timescale.created) AS created,
+SELECT date_trunc(:group_period, timescale.ended) AS ended,
     ROUND(AVG(COALESCE(num_chats, 0))) as metric_value
 FROM (
         SELECT date_trunc(
@@ -27,8 +27,8 @@ FROM (
                     NOW(),
                     '1 day'::INTERVAL
                 )
-            ) AS created
+            ) AS ended
     ) AS timescale
-    LEFT JOIN chats ON chats.created = timescale.created
+    LEFT JOIN chats ON chats.ended = timescale.ended
 GROUP BY 1
 ORDER BY 1 DESC
