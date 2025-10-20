2399790ef793a10f79acbcf1e2f0195f8a42a5e2
Varmo <101868197+varmoh@users.noreply.github.com>
2025-10-20 14:44:01 +0300

    Update SQL query to filter chat records from 3 years

--- Resql/backoffice/POST/get-dead-chat-ids.sql
+++ Resql/backoffice/POST/get-dead-chat-ids.sql
@@ -2,8 +2,8 @@ SELECT array_agg(base_id) AS base_ids
 FROM (
          SELECT DISTINCT ON (base_id) base_id, status, updated
          FROM chat
-         WHERE updated >= (CURRENT_DATE - INTERVAL '1 hour')
-           AND updated < CURRENT_DATE + INTERVAL '1 hour'
+         WHERE updated >= (CURRENT_DATE - INTERVAL '3 years')
+           AND updated < CURRENT_DATE
          ORDER BY base_id, updated DESC
      ) latest
 WHERE latest.status IS NOT NULL
