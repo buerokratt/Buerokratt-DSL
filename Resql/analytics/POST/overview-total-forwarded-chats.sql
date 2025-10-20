ca819bd223730e3d44e09aa2e1e1e86adf8831df
GitHub Action <action@github.com>
2025-10-20 10:20:29 +0000

    Aggregated files from source repos [2025-10-20]

--- Resql/analytics/POST/overview-total-forwarded-chats.sql
+++ Resql/analytics/POST/overview-total-forwarded-chats.sql
@@ -1,23 +1,27 @@
-WITH forwarded_chats_by_csa AS (
-    SELECT base_id, customer_support_id, customer_support_display_name, ended
-    FROM chat
-    WHERE status = 'REDIRECTED'
-      AND (
-        :showTest = TRUE
-            OR chat.test = FALSE
-        )
-      AND (
-        array_length(ARRAY[:urls]::text[], 1) IS NULL
-            OR (array_length(ARRAY[:urls]::text[], 1) = 1 AND (ARRAY[:urls]::text[])[1] = 'none')
-            OR chat.end_user_url LIKE ANY(ARRAY[:urls]::text[])
-        )
-      AND customer_support_id <> ''
-      AND customer_support_id IS NOT NULL
-    GROUP BY base_id, customer_support_id, customer_support_display_name, ended
+WITH latest_chat AS (
+    SELECT DISTINCT ON (base_id)
+    base_id,
+    status,
+    external_id,
+    ended,
+    end_user_url,
+    test
+FROM chat
+WHERE (:showTest = TRUE OR chat.test = FALSE)
+  AND (
+    array_length(ARRAY[:urls]::text[], 1) IS NULL
+    OR (array_length(ARRAY[:urls]::text[], 1) = 1 AND (ARRAY[:urls]::text[])[1] = 'none')
+    OR chat.end_user_url LIKE ANY(ARRAY[:urls]::text[]))
+ORDER BY base_id, ended DESC
+    ),
+    redirected_chats AS (
+SELECT DISTINCT base_id, updated
+FROM chat
+WHERE status = 'REDIRECTED'
 )
 SELECT COUNT(DISTINCT base_id) AS metric_value
-FROM forwarded_chats_by_csa
-WHERE date_trunc('day', forwarded_chats_by_csa.ended) = date_trunc('day', current_date - '1 day'::INTERVAL)
+FROM redirected_chats
+WHERE date_trunc('day', redirected_chats.updated) = date_trunc('day', current_date - '1 day'::INTERVAL)
 UNION ALL
 SELECT COUNT(DISTINCT base_id) FILTER (
         WHERE external_id IS NOT NULL AND external_id <> ''
@@ -28,7 +32,7 @@ WHERE (
         OR chat.test = FALSE
     )
   AND (
-    array_length(ARRAY[:urls]::TEXT[], 1) IS NULL
-        OR chat.end_user_url LIKE ANY(ARRAY[:urls]::TEXT[])
-    )
+    array_length(ARRAY[:urls]::text[], 1) IS NULL
+        OR (array_length(ARRAY[:urls]::text[], 1) = 1 AND (ARRAY[:urls]::text[])[1] = 'none')
+        OR chat.end_user_url LIKE ANY(ARRAY[:urls]::text[]))
   AND date_trunc('day', chat.ended) = date_trunc('day', current_date - '1 day'::INTERVAL);
