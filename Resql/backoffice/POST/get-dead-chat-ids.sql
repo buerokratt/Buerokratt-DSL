2fe27397b5cf12303ac69688ca584e67d2c279f3
ffrose <119657383+ffrose@users.noreply.github.com>
2025-11-06 12:37:38 +0200

    Update get-dead-chat-ids.sql

--- Resql/backoffice/POST/get-dead-chat-ids.sql
+++ Resql/backoffice/POST/get-dead-chat-ids.sql
@@ -1,9 +1,9 @@
 SELECT array_agg(base_id) AS base_ids
 FROM (
-         SELECT DISTINCT ON (base_id) base_id, status, updated
+         SELECT DISTINCT ON (base_id) base_id, status, updated, ended
          FROM chat
          WHERE updated >= (CURRENT_DATE - INTERVAL '1 week')
-           AND updated < CURRENT_DATE + INTERVAL '1 week'
+           AND updated < CURRENT_DATE + INTERVAL '1 day'
          ORDER BY base_id, updated DESC
      ) latest
 WHERE latest.status IS NOT NULL
