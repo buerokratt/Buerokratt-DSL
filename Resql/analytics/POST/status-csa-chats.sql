b326427849e70af1bd97a84a1b3d8c1095271ec8
ffrose <119657383+ffrose@users.noreply.github.com>
2026-02-25 13:11:47 +0200

    Update status-csa-chats.sql

--- Resql/analytics/POST/status-csa-chats.sql
+++ Resql/analytics/POST/status-csa-chats.sql
@@ -1,20 +1,28 @@
+WITH latest_messages AS (
+    SELECT DISTINCT ON (message.chat_base_id)
+        message.chat_base_id,
+        message.event
+    FROM message
+    WHERE message.event IN (:events)
+        AND message.author_role = 'backoffice-user'
+    ORDER BY message.chat_base_id, message.updated DESC
+)
 SELECT
     date_trunc(:metric, chat.ended) AS date_time,
-    message.event AS event,
+    lm.event AS event,
     COUNT(DISTINCT chat.base_id)
 FROM chat
-         JOIN message ON chat.base_id = message.chat_base_id
+    JOIN latest_messages lm
+        ON chat.base_id = lm.chat_base_id
 WHERE chat.status = 'ENDED'
-  AND message.event IN (:events)
-  AND message.author_role = 'backoffice-user'
-  AND (
-    array_length(ARRAY[:urls]::TEXT[], 1) IS NULL
+    AND (
+        array_length(ARRAY[:urls]::TEXT[], 1) IS NULL
         OR chat.end_user_url LIKE ANY(ARRAY[:urls]::TEXT[])
     )
-  AND (
-    :showTest = TRUE
+    AND (
+        :showTest = TRUE
         OR chat.test = FALSE
     )
-  AND chat.ended::timestamptz BETWEEN :start::timestamptz AND :end::timestamptz
+    AND chat.ended::timestamptz BETWEEN :start::timestamptz AND :end::timestamptz
 GROUP BY date_time, event
 ORDER BY event;
