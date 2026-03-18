6728f1c96ecdebdcaa7b62350dca9b063c7f41b2
Varmo <101868197+varmoh@users.noreply.github.com>
2026-03-18 11:25:17 +0200

    Fix SQL syntax for chat status condition

--- Resql/backoffice/POST/get-smax-chat.sql
+++ Resql/backoffice/POST/get-smax-chat.sql
@@ -4,13 +4,13 @@ FROM
     chat AS c
 WHERE
     c.base_id = :chatBaseId
-    AND c.status = 'ENDED'
     AND c.created > COALESCE((
         SELECT MAX(created)
         FROM chat
         WHERE base_id = :chatBaseId
           AND status = 'ENDED'
     ), TIMESTAMP '1970-01-01 00:00:00')
+    AND c.status = 'ENDED'
 ORDER BY
     c.created ASC
 LIMIT 1;
