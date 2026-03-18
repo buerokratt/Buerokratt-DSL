46902efd5656f894d59cb01d813b98e50cba5197
Varmo <101868197+varmoh@users.noreply.github.com>
2026-03-18 11:38:26 +0200

    Update get-smax-chat.sql

--- Resql/backoffice/POST/get-smax-chat.sql
+++ Resql/backoffice/POST/get-smax-chat.sql
@@ -1,15 +1,18 @@
 SELECT
-    c.*
+    *
 FROM
-    chat c
+    chat AS c
 WHERE
+    c.created > COALESCE((
+        SELECT
+            created
+        FROM
+            chat
+        WHERE
+            base_id = :chatBaseId AND
+            status = 'ENDED'
+    ), '1970-01-01') AND
     c.status = 'ENDED'
-    AND NOT EXISTS (
-        SELECT 1
-        FROM chat_smax_synchronization s
-        WHERE s.chat_base_id = c.base_id
-          AND s.status = 'SUCCESS'
-    )
 ORDER BY
-    c.created ASC
+    created ASC
 LIMIT 1;
