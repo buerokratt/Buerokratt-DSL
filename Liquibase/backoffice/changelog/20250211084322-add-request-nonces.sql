b15662a9bb09c8be7807756394e5bca7a39ec0a6
ffrose <119657383+ffrose@users.noreply.github.com>
2026-04-27 16:39:04 +0300

    Delete Liquibase/backoffice/changelog/20250211084322-add-request-nonces.sql

--- Liquibase/backoffice/changelog/20250211084322-add-request-nonces.sql
+++
@@ -1,8 +0,0 @@
--- liquibase formatted sql
--- changeset jannostern:20250211084322
-
-CREATE TABLE request_nonces(
-        nonce varchar(36) DEFAULT (gen_random_uuid()),
-        created_at TIMESTAMP WITH TIME ZONE DEFAULT (now()),
-        valid_until TIMESTAMP WITH TIME ZONE,
-        used_at TIMESTAMP );
