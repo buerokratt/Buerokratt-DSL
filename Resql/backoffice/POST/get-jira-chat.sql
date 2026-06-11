d9f2547305aba145ebb3413f1fb18e814be54980
Varmo <101868197+varmoh@users.noreply.github.com>
2026-06-11 11:28:39 +0300

    Update get-jira-chat.sql

--- Resql/backoffice/POST/get-jira-chat.sql
+++ Resql/backoffice/POST/get-jira-chat.sql
@@ -1,18 +1,14 @@
-SELECT
-    *
-FROM
-    chat AS c
-WHERE
-    c.created > COALESCE((
-        SELECT
-            created
-        FROM
-            chat
-        WHERE
-            base_id = :chatBaseId AND
-            status = 'ENDED'
-    ), '1970-01-01') AND
-    c.status = 'ENDED'
-ORDER BY
-    created ASC
+SELECT *
+FROM chat c
+WHERE c.created > COALESCE(
+    (
+        SELECT MAX(c2.created)
+        FROM chat c2
+        WHERE c2.base_id = ?
+          AND c2.status = 'ENDED'
+    ),
+    TIMESTAMP '1970-01-01 00:00:00'
+)
+AND c.status = 'ENDED'
+ORDER BY c.created ASC
 LIMIT 1;
