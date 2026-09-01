559e6b87178442329a98aa55a35741dd886c5f6d
ffrose <119657383+ffrose@users.noreply.github.com>
2026-09-01 13:35:08 +0300

    Update aggregate-files.sh

--- scripts/aggregate-files.sh
+++ scripts/aggregate-files.sh
@@ -13,8 +13,8 @@ SOURCE_REPOS=(
   "buerokratt/Training-Module:v3.3.1"
   "buerokratt/Analytics-Module:v3.3.2"
   "buerokratt/Service-Module:v3.3.2"
-  "buerokratt/Common-Services:v3.3.2"
-  "buerokratt/CronManager:v3.3.2"
+  "buerokratt/Common-Services:v3.3.1"
+  "buerokratt/CronManager:v3.3.1"
   "buerokratt/Common-Knowledge:wip"
 )
 
