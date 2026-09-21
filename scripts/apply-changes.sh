801c142c694a0a817083d31d7bf65b3ff90db511
ffrose <119657383+ffrose@users.noreply.github.com>
2026-09-21 11:04:52 +0300

    Update apply-changes.sh

--- scripts/apply-changes.sh
+++ scripts/apply-changes.sh
@@ -1,30 +1,6 @@
 #!/bin/bash
 set -e
 
-if [ -f "Liquibase/analytics/changelog.yaml" ]; then
-  sed -i \
-    's|changelog/changelog/|changelog/|g' \
-    Liquibase/analytics/changelog.yaml
-fi
-
-if [ -f "Liquibase/training/changelog.yaml" ]; then
-  sed -i \
-    's|changelog/changelog|changelog|g' \
-    Liquibase/training/changelog.yaml
-fi
-
-if [ -f "Liquibase/services/liquibase.properties" ]; then
-  sed -i \
-    's|/changelog/changelog.yaml|/changelog.yaml|g' \
-    Liquibase/services/liquibase.properties
-fi
-
-if [ -f "Liquibase/analytics/liquibase.properties" ]; then
-  sed -i \
-    's|changelog/changelog|changelog|g' \
-    Liquibase/analytics/liquibase.properties
-fi
-
 if [ -f "Ruuter/private/v2/training/GET/internal/return-file-locations.yml" ]; then
   sed -i \
     "s|/DMapper/training/locations/|/locations/|g" \
